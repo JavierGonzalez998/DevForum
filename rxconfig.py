8a2ca437de9f221bbbf406de491bb07aeea2ceec
Javier Gonzalez <javiergonzaleze98@gmail.com>
2024-05-02 17:29:19 -0400

    add caddy and nixpacks

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv,find_dotenv
 
 
+railway_domain = "RAILWAY_PUBLIC_DOMAIN"
+
 load_dotenv(find_dotenv())
 
 db = os.getenv("POSTGRE_DB_CONN")
@@ -16,4 +18,6 @@ config = ReflextemplateConfig(
     db_url=f"postgresql+psycopg2://{db}",
     frontend_port=3000, # default frontend port
     backend_port=8000, # default backend port
+    # use https and the railway public domain with a backend route if available, otherwise default to a local address
+    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
