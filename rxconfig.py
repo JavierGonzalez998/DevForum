ce862b0ae802439d4ed2f86548b4cab5ab7b7b2e
Javier Gonzalez <javiergonzaleze98@gmail.com>
2024-05-18 10:05:31 -0400

    fixes on netlify archive

--- rxconfig.py
+++ rxconfig.py
@@ -1,17 +1,7 @@
 import reflex as rx
-import os
 
-
-env="PROD"
-
-railway_domain = "RAILWAY_PUBLIC_DOMAIN"
-db = "postgres:msemSBRKuEuguWFvsMDsqczpUBGGqCGn@viaduct.proxy.rlwy.net:41996/railway" if os.environ.get("DATABASE_URL") is None else os.environ.get("DATABASE_URL")
-
-val = db if env == "PROD" else "postgres:admin12345@localhost:5432/postgres"
 
 conn = "sqlite:///DevForum/Backend/Database/database.db"
-
-print(conn)
 
 class ReflextemplateConfig(rx.Config):
     pass
@@ -21,6 +11,4 @@ config = ReflextemplateConfig(
     db_url=conn,
     frontend_port=3000, # default frontend port
     backend_port=8000, # default backend port
-    # use https and the railway public domain with a backend route if available, otherwise default to a local address
-    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
